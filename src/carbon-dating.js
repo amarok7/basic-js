927641c401bd1213c023e3e56cce77eea5858f7f
anutaguzova <anuta-guzova@mail.ru>
2020-10-28 18:36:21 +0300

    feat:add carbondating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== 'string' || Number(sampleActivity) == NaN || Number(sampleActivity) == 0 || Number(sampleActivity) > MODERN_ACTIVITY || Number(sampleActivity) <= 0) {
+  if (typeof sampleActivity !== 'string' || isNaN(Number(sampleActivity)) || Number(sampleActivity) === 0 || Number(sampleActivity) > MODERN_ACTIVITY || Number(sampleActivity) < 0) {
     return false
   } else {
     return Math.ceil(Math.log((+sampleActivity)/MODERN_ACTIVITY) / (0.693 / HALF_LIFE_PERIOD))

0cb29cc4d0d13797b192e12b835749b9c8f38933
anutaguzova <anuta-guzova@mail.ru>
2020-10-29 11:14:34 +0300

    feat:add solution extended sep

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,13 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  if(typeof str !== 'string') String(str);
-  if(typeof options.addition !== 'string') String(options.addition);
+
+  if (typeof str !== 'string') String(str);
+  if (!options.hasOwnProperty('addition')) {
+    options.addition = '';
+  }
+  if (typeof options.addition !== 'string') {
+    options.addition = String(options.addition);
+  }
+
   options.separator = options.separator || '+'
   options.additionSeparator = options.additionSeparator || '|'
-  if(options.additionRepeatTimes == 'undefined') (str + options.addition + options.separator).repeat(options.repeatTimes-1) + str + options.addition
-  if(options.repeatTimes == 'undefined') str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition
-  return (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition + options.separator).repeat(options.repeatTimes-1) + str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition
+  return (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) +
+    options.addition + options.separator).repeat(options.repeatTimes - 1) +
+    str +
+    (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1) +
+    options.addition
 };
 
 

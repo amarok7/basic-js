bdc7c27e2008e0c658428eb690ea3e79345060fd
anutaguzova <anuta-guzova@mail.ru>
2020-11-01 21:13:45 +0300

    feat:add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error();
   }
 }
 
